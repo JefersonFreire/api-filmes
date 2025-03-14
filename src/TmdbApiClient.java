daa49d1c982f6effa56430f093ed5cf3e4d010db
JefersonFreire <jfreire86@hotmail.com>
2025-03-14 13:28:11 -0300

    criando classe propria para chamada da api

---
+++ src/TmdbApiClient.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class TmdbApiClient {
+
+    private final String apikey;
+    private final int page;
+
+    public TmdbApiClient(String apikey, int page) {
+        this.apikey = apikey;
+        this.page = page;
+    }
+
+    public String getBody(){
+
+
+
+        try {
+            URI apiTMDB = URI.create("https://api.themoviedb.org/3/movie/top_rated");
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+
+                    .uri(URI.create(apiTMDB + "?language=pt-BR&page=" + page))
+                    .GET()
+                    .header("Accept", "application/json")
+                    .header("Authorization", "Bearer " + apikey)
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+           return response.body();
+        }catch (IOException | InterruptedException e){
+            throw new IllegalStateException(e);
+        }
+    }
+}
+
